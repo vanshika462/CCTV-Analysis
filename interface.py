72c436156fdfc7e618e6cac92e8baefcc1014ccf
vanshika462 <vanshikashrivastava462@gmail.com>
2023-11-24 14:21:54 +0530

    interface+adjusting size ratio

---
+++ interface.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk  # Pillow library for working with images
+import cv2
+import numpy as np
+
+# Placeholder function for activity detection using the model
+def detect_activity(video_path, activity_type):
+    # Replace this with your actual model code
+    # For simplicity, this example just displays a message
+    return f"Detecting {activity_type} in {video_path}"
+
+# Function to open a video file and display the first frame
+def open_video():
+    file_path = filedialog.askopenfilename(filetypes=[("Video files", "*.mp4;*.avi")])
+    if file_path:
+        cap = cv2.VideoCapture(file_path)
+        ret, frame = cap.read()
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(frame)
+            img.thumbnail((400, 400))
+            img = ImageTk.PhotoImage(img)
+            video_label.config(image=img)
+            video_label.image = img
+            video_label.file_path = file_path
+        cap.release()
+
+# Function to perform activity detection based on the selected option
+def detect_selected_activity():
+    if video_label.file_path:
+        selected_activity = activity_var.get()
+        result = detect_activity(video_label.file_path, selected_activity)
+        result_label.config(text=result)
+
+# Main GUI window
+root = tk.Tk()
+root.title("Activity Detection Interface")
+
+# Video display label
+video_label = tk.Label(root)
+video_label.pack(pady=10)
+
+# Open Video Button
+open_button = tk.Button(root, text="Open Video", command=open_video)
+open_button.pack(pady=10)
+
+# Activity selection
+activities = ["Loitering", "Peak Hour Pedestrian Traffic", "Custom Activity"]
+activity_var = tk.StringVar(root)
+activity_var.set(activities[0])  # Default activity
+activity_menu = tk.OptionMenu(root, activity_var, *activities)
+activity_menu.pack(pady=10)
+
+# Detect Activity Button
+detect_button = tk.Button(root, text="Detect Activity", command=detect_selected_activity)
+detect_button.pack(pady=10)
+
+# Result label
+result_label = tk.Label(root, text="")
+result_label.pack(pady=10)
+
+root.mainloop()
