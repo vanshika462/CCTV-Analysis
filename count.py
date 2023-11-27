f2d9f703ebd8297d9a2000b42853e5931bee89ea
unknown <sarahnasim2016@gmail.com>
2023-11-27 23:27:42 +0530

    frames viewing in interface and count object button added

--- count.py
+++ count.py
@@ -1,76 +1,94 @@
+# import cv2
+# import numpy as np
+
+# def count_objects(video_path):
+#     net = cv2.dnn.readNet("C:\\Users\\WelCome\\CCTV-Analysis\\darknet\\yolov3.weights", "C:\\Users\\WelCome\\CCTV-Analysis\\darknet\\cfg\\yolov3.cfg")
+#     layer_names = net.getLayerNames()
+#     output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+
+#     classes = []
+#     with open("darknet\data\coco.names", "r") as f:
+#         classes = [line.strip() for line in f.readlines()]
+
+#     cap = cv2.VideoCapture(video_path)
+#     object_count = 0
+
+#     while True:
+#         ret, frame = cap.read()
+#         if not ret:
+#             break
+
+#         height, width, channels = frame.shape
+#         blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+#         net.setInput(blob)
+#         outs = net.forward(output_layers)
+
+#         # Initialize lists to store bounding boxes, confidences, and class IDs
+#         boxes = []
+#         confidences = []
+#         class_ids = []
+
+#         for out in outs:
+#             for detection in out:
+#                 scores = detection[5:]
+#                 class_id = np.argmax(scores)
+#                 confidence = scores[class_id]
+
+#                 if confidence > 0.75:
+#                     # Increment object count
+#                     object_count += 1
+
+#         cv2.waitKey(1)  # Adjust the waitKey delay if needed
+
+#     cap.release()
+#     cv2.destroyAllWindows()
+
+#     return object_count
+
+
 import cv2
 import numpy as np
 
-net = cv2.dnn.readNet("C:\\Users\\WelCome\\CCTV-Analysis\\darknet\\yolov3.weights", "C:\\Users\\WelCome\\CCTV-Analysis\\darknet\\cfg\\yolov3.cfg")
-layer_names = net.getLayerNames()
-output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+def count_objects(video_path):
+    net = cv2.dnn.readNet("C:\\Users\\WelCome\\CCTV-Analysis\\darknet\\yolov3.weights", "C:\\Users\\WelCome\\CCTV-Analysis\\darknet\\cfg\\yolov3.cfg")
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 
-classes = []
-with open("darknet\data\coco.names", "r") as f:
-    classes = [line.strip() for line in f.readlines()]
+    classes = []
+    with open("darknet\data\coco.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()]
 
-# Read a single frame (replace 'your_frame_path.jpg' with the actual path)
-frame = cv2.imread('C:\\Users\\WelCome\\CCTV-Analysis\\frames\\ch4_20230824164618\\frame_0340.jpg')
+    cap = cv2.VideoCapture(video_path)
+    object_count = 0
 
-height, width, channels = frame.shape
-blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-net.setInput(blob)
-outs = net.forward(output_layers)
+        height, width, channels = frame.shape
+        blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+        net.setInput(blob)
+        outs = net.forward(output_layers)
 
-# Initialize lists to store bounding boxes, confidences, and class IDs
-boxes = []
-confidences = []
-class_ids = []
+        # Initialize lists to store bounding boxes, confidences, and class IDs
+        boxes = []
+        confidences = []
+        class_ids = []
 
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
 
-        if confidence > 0.75:
-            # Scale the bounding box back to the original image size
-            center_x = int(detection[0] * width)
-            center_y = int(detection[1] * height)
-            w = int(detection[2] * width)
-            h = int(detection[3] * height)
+                if confidence > 0.75:
+                    # Increment object count
+                    object_count += 1
 
-            # Rectangle coordinates
-            x = int(center_x - w / 2)
-            y = int(center_y - h / 2)
+        cv2.waitKey(1)  # Adjust the waitKey delay if needed
 
-            boxes.append([x, y, w, h])
-            confidences.append(float(confidence))
-            class_ids.append(class_id)
+    cap.release()
+    cv2.destroyAllWindows()
 
-# Apply Non-Maximum Suppression (NMS)
-indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-
-# Initialize dictionary to store label counts for the current frame
-label_counts = {class_name: 0 for class_name in classes}
-
-# Process and draw filtered detections
-for i in range(len(boxes)):
-    if i in indexes:
-        x, y, w, h = boxes[i]
-        label = str(classes[class_ids[i]])
-        confidence = confidences[i]
-
-        # Update class count
-        label_counts[label] += 1
-
-        color = (0, 255, 0)  # Green color for the bounding box
-        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-        cv2.putText(frame, f"{label} {confidence:.2f}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-# Print the label counts for the current frame
-for class_name, count in label_counts.items():
-    if count > 0:
-        print(f"{class_name}: {count} instances detected")
-
-# Create a window with the original frame size
-cv2.namedWindow("Object Detection", cv2.WINDOW_NORMAL)
-cv2.imshow("Object Detection", frame)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    return object_count
--- interface.py
+++ interface.py
@@ -1,20 +1,24 @@
 import tkinter as tk
 from tkinter import filedialog
-from PIL import Image, ImageTk  # Pillow library for working with images
+from PIL import Image, ImageTk
 import cv2
-import numpy as np
+from count import count_objects
 
 # Placeholder function for activity detection using the model
-def detect_activity(video_path, activity_type):
+def detect_activity(input_path, activity_type):
     # Replace this with your actual model code
     # For simplicity, this example just displays a message
-    return f"Detecting {activity_type} in {video_path}"
+    return f"Detecting {activity_type} in {input_path}"
 
-# Function to open a video file and display the first frame
-def open_video():
-    file_path = filedialog.askopenfilename(filetypes=[("Video files", "*.mp4;*.avi")])
+# Function to open a video file or an image file and display the first frame
+def open_input():
+    file_path = filedialog.askopenfilename(filetypes=[("Video files", "*.mp4;*.avi"), ("Image files", "*.jpg;*.jpeg;*.png")])
     if file_path:
         cap = cv2.VideoCapture(file_path)
+
+        if not cap.isOpened():
+            return
+
         ret, frame = cap.read()
         if ret:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -24,6 +28,7 @@ def open_video():
             video_label.config(image=img)
             video_label.image = img
             video_label.file_path = file_path
+
         cap.release()
 
 # Function to perform activity detection based on the selected option
@@ -33,6 +38,12 @@ def detect_selected_activity():
         result = detect_activity(video_label.file_path, selected_activity)
         result_label.config(text=result)
 
+# Function to count objects in the video or frame
+def count_objects_in_input():
+    if video_label.file_path:
+        object_count = count_objects(video_label.file_path)
+        object_count_label.config(text=f"Object Count: {object_count}")
+
 # Main GUI window
 root = tk.Tk()
 root.title("Activity Detection Interface")
@@ -41,9 +52,13 @@ root.title("Activity Detection Interface")
 video_label = tk.Label(root)
 video_label.pack(pady=10)
 
-# Open Video Button
-open_button = tk.Button(root, text="Open Video", command=open_video)
+# Open Input Button
+open_button = tk.Button(root, text="Open File", command=open_input)
 open_button.pack(pady=10)
+
+# Count Objects Button
+count_objects_button = tk.Button(root, text="Count Objects", command=count_objects_in_input)
+count_objects_button.pack(pady=10)
 
 # Activity selection
 activities = ["Loitering", "Peak Hour Pedestrian Traffic", "Custom Activity"]
@@ -59,5 +74,9 @@ detect_button.pack(pady=10)
 # Result label
 result_label = tk.Label(root, text="")
 result_label.pack(pady=10)
+
+# # Object count label
+# object_count_label = tk.Label(root, text="Object Count: ")
+# object_count_label.pack(pady=10)
 
 root.mainloop()
